e948597fe3be61bd0996aec26023160f83d4e6bb
gongym <1967988842@qq.com>
2019-04-20 18:50:29 +0800

    [版本升级]模板升级，导致所有模块升级

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <!--suppress CheckEmptyScriptTag -->
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
     <div class="main-container">
-      <navbar />
+      <div :class="{'fixed-header':fixedHeader}">
+        <navbar />
+      </div>
       <app-main />
     </div>
   </div>
@@ -29,6 +30,9 @@ export default {
     device() {
       return this.$store.state.app.device
     },
+    fixedHeader() {
+      return this.$store.state.settings.fixedHeader
+    },
     classObj() {
       return {
         hideSidebar: !this.sidebar.opened,
@@ -46,8 +50,10 @@ export default {
 }
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped>
-  @import "src/styles/mixin.scss";
+<style lang="scss" scoped>
+  @import "~@/styles/mixin.scss";
+  @import "~@/styles/variables.scss";
+
   .app-wrapper {
     @include clearfix;
     position: relative;
@@ -66,5 +72,22 @@ export default {
     height: 100%;
     position: absolute;
     z-index: 999;
+  }
+
+  .fixed-header {
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 9;
+    width: calc(100% - #{$sideBarWidth});
+    transition: width 0.28s;
+  }
+
+  .hideSidebar .fixed-header {
+    width: calc(100% - 54px)
+  }
+
+  .mobile .fixed-header {
+    width: 100%;
   }
 </style>
