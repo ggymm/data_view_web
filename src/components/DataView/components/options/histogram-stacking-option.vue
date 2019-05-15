6384b444e3607bbaf9e8525d0f332ce8c84bf8b2
wulantana <wltn@ninelock.com>
2019-05-15 17:56:21 +0800

    [日常更新]堆叠柱状图--加入option

--- src/components/DataView/components/options/histogram-stacking-option.vue
+++ src/components/DataView/components/options/histogram-stacking-option.vue
@@ -32,8 +32,271 @@
               </el-form-item>
             </el-form>
           </el-collapse-item>
-          <el-collapse-item title="图表配置">
-            <el-form ref="form" :model="item" label-width="35%" size="mini" />
+          <el-collapse-item title="图表样式">
+            <el-form ref="form" :model="item" label-width="35%" size="mini">
+              <el-form-item label="图表Grid(顶)">
+                <el-col :span="20">
+                  <el-input v-model="item.option.grid.top" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="图表Grid(左)">
+                <el-col :span="20">
+                  <el-input v-model="item.option.grid.left" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="图表Grid(右)">
+                <el-col :span="20">
+                  <el-input v-model="item.option.grid.right" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="图表Grid(底)">
+                <el-col :span="20">
+                  <el-input v-model="item.option.grid.bottom" />
+                </el-col>
+              </el-form-item>
+            </el-form>
+          </el-collapse-item>
+          <el-collapse-item title="图表标题">
+            <el-form ref="form" :model="item" label-width="35%" size="mini">
+              <el-form-item label="显示标题">
+                <el-col :span="20">
+                  <el-select v-model="item.option.title.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.title.show" label="标题">
+                <el-col :span="20">
+                  <el-input v-model="item.option.title.text" />
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.title.show" label="标题颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.title.textStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.title.show" label="标题位置">
+                <el-col :span="20">
+                  <el-select v-model="item.option.title.left">
+                    <el-option
+                      v-for="position in positionList"
+                      :key="position.value"
+                      :label="position.label"
+                      :value="position.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+            </el-form>
+          </el-collapse-item>
+          <el-collapse-item title="横坐标样式">
+            <el-form ref="form" :model="item" label-width="35%" size="mini">
+              <el-form-item label="显示文字">
+                <el-col :span="20">
+                  <el-select v-model="item.option.xAxis.axisLabel.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.xAxis.axisLabel.show" label="坐标名称">
+                <el-col :span="20">
+                  <el-input v-model="item.option.xAxis.name" />
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.xAxis.axisLabel.show" label="名称颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.xAxis.axisLabel.textStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="显示坐标线">
+                <el-col :span="20">
+                  <el-select v-model="item.option.xAxis.axisLine.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.xAxis.axisLine.show" label="坐标线颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.xAxis.axisLine.lineStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="显示分割线">
+                <el-col :span="20">
+                  <el-select v-model="item.option.xAxis.splitLine.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.xAxis.splitLine.show" label="分割线颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.xAxis.splitLine.lineStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="标签旋转角度">
+                <el-col :span="20">
+                  <el-input-number v-model="item.option.xAxis.axisLabel.rotate" :min="-90" :max="90" />
+                </el-col>
+              </el-form-item>
+            </el-form>
+          </el-collapse-item>
+          <el-collapse-item title="纵坐标样式">
+            <el-form ref="form" :model="item" label-width="35%" size="mini">
+              <el-form-item label="显示文字">
+                <el-col :span="20">
+                  <el-select v-model="item.option.yAxis.axisLabel.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.yAxis.axisLabel.show" label="坐标名称">
+                <el-col :span="20">
+                  <el-input v-model="item.option.yAxis.name" />
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.yAxis.axisLabel.show" label="名称颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.yAxis.axisLabel.textStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="显示坐标线">
+                <el-col :span="20">
+                  <el-select v-model="item.option.yAxis.axisLine.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.yAxis.axisLine.show" label="坐标线颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.yAxis.axisLine.lineStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="显示分割线">
+                <el-col :span="20">
+                  <el-select v-model="item.option.yAxis.splitLine.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.yAxis.splitLine.show" label="分割线颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.yAxis.splitLine.lineStyle.color" />
+                </el-col>
+              </el-form-item>
+              <el-form-item label="显示分割区域">
+                <el-col :span="20">
+                  <el-select v-model="item.option.yAxis.splitArea.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.yAxis.splitArea.show" label="分割区域颜色">
+                <el-col :span="10">
+                  <el-color-picker v-model="item.option.yAxis.splitArea.areaStyle.color[0]" />
+                </el-col>
+                <el-col :span="10">
+                  <el-color-picker v-model="item.option.yAxis.splitArea.areaStyle.color[1]" />
+                </el-col>
+              </el-form-item>
+            </el-form>
+          </el-collapse-item>
+          <el-collapse-item title="图例样式">
+            <el-form ref="form" :model="item" label-width="35%" size="mini">
+              <el-form-item label="显示图例">
+                <el-col :span="20">
+                  <el-select v-model="item.option.legend.show">
+                    <el-option
+                      v-for="isShow in isShowList"
+                      :key="isShow.value"
+                      :label="isShow.label"
+                      :value="isShow.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.legend.show" label="图例布局朝向">
+                <el-col :span="20">
+                  <el-select v-model="item.option.legend.orient">
+                    <el-option
+                      v-for="orient in orientList"
+                      :key="orient.value"
+                      :label="orient.label"
+                      :value="orient.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.legend.show" label="图例位置(左)">
+                <el-col :span="20">
+                  <el-input-number v-model="item.option.legend.left" :min="0" />
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.legend.show" label="图例位置(上)">
+                <el-col :span="20">
+                  <el-input-number v-model="item.option.legend.top" :min="0" />
+                </el-col>
+              </el-form-item>
+              <el-form-item v-if="item.option.legend.show" label="文字颜色">
+                <el-col :span="20">
+                  <el-color-picker v-model="item.option.legend.textStyle.color" />
+                </el-col>
+              </el-form-item>
+            </el-form>
+          </el-collapse-item>
+          <el-collapse-item v-if="item.option.series.length !== 0" title="图表曲线样式">
+            <el-form ref="form" :model="item" label-width="35%" size="mini">
+              <el-form-item v-for="(series, index) in item.option.series" :key="series.name" :label="'是否平滑(' + (index + 1) + ')'">
+                <el-col :span="20">
+                  <el-select v-model="series.smooth">
+                    <el-option
+                      v-for="smooth in smoothList"
+                      :key="smooth.value"
+                      :label="smooth.label"
+                      :value="smooth.value"
+                    />
+                  </el-select>
+                </el-col>
+              </el-form-item>
+            </el-form>
           </el-collapse-item>
           <el-button size="mini" type="danger" @click="handleDelete">删除图表</el-button>
         </el-collapse>
