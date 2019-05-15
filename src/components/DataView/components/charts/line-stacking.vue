898658aa3e5aacd086e38d0938c65a270499fe94
wulantana <wltn@ninelock.com>
2019-05-15 17:53:01 +0800

    [日常更新]修改堆叠折线图、堆叠柱状图

--- src/components/DataView/components/charts/line-stacking.vue
+++ src/components/DataView/components/charts/line-stacking.vue
@@ -56,12 +56,11 @@ export default {
     setData() {
       this.option.dataset.source = this.apiData
       var size = this.apiData[0].length
-      if (this.option.series.length === 0) {
-        for (var i = 0; i < size - 1; i++) {
-          this.option.series.push({
-            'type': 'line'
-          })
-        }
+      this.option.series = {}
+      for (var i = 0; i < size - 1; i++) {
+        this.option.series.push({
+          'type': 'line'
+        })
       }
     }
   }
